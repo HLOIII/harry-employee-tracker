4ad142c5bda418904a37390bcd189e635d118fab
Harry <chairmanharryogletree@gmail.com>
2022-04-25 19:26:03 -0400

    commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,6 @@
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS department;
-
+DROP DATABASE IF EXISTS employeeTracker;
+CREATE DATABASE employeeTracker;
+USE employeeTracker;
 
 
 CREATE TABLE department (
@@ -12,17 +11,14 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(19,2) NOT NULL,
-    department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,58 @@
-INSERT INTO department (name)
-VALUE
-('Mechanical'),
-('Test'),
-('QA'),
-('Shipping'),
-('Stockroom'),
-('SMT'),
-('2_OP');
+USE employeeTracker;
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO department
+(name)
 VALUES
-('Assembler', 100, 1),
-('Technician', 300, 2),
-('Inspector', 200, 3),
-('Mover', 100, 4),
-('Material Handler', 150.5, 5),
-('Operator', 105.6, 6),
-('Mechanical Engineer', 400, 7);
+('Business'),
+('Creative'),
+('Guest/Customer Service'),
+('Innovation and Technology'),
+('Production and Entertainment'),
+('Marketing and Communication');
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO role
+(title, salary, department_id)
 VALUES
-('John', 'Lin', 7, NULL),
-('Carlos', 'Dominguez', 4, 1),
-('Jason', 'Hernandez', 6, 2),
-('Elizabeth', 'Tracy', 6, 1),
-('Jeff', 'Ferrel', 7, NULL);
+('Administrative', 40000.00, 1),
+('Business Development', 40000.00, 1),
+('Compliance', 40000.00, 1),
+('Human Resources', 40000.00, 1),
+('Animation', 50000.00, 2),
+('Content Development', 50000.00, 2),
+('Digital and Interactive Media', 50000.00, 2),
+('Gaming', 50000.00, 2),
+('Call Center', 30000.00, 3),
+('Food and Beverage', 30000.00, 3),
+('Hotel', 30000.00, 3),
+('Cruise Ship Jobs', 30000.00, 3),
+('Park Operations', 30000.00, 3),
+('Recreation', 30000.00, 3),
+('Retail Operations', 30000.00, 3),
+('Data', 60000.00, 4),
+('Engineering', 60000.00, 4),
+('Medical', 60000.00, 4),
+('Quality Assurance', 60000.00, 4),
+('Research', 60000.00, 4),
+('Research and Development', 60000.00, 4),
+('Sciences', 60000.00, 4),
+('Supply Chain Management', 60000.00, 4),
+('Technology', 60000.00, 4),
+('Broadcast', 50000.00, 5),
+('Production', 50000.00, 5),
+('Studio Operations', 50000.00, 5),
+('Communications', 30000.00, 6),
+('Creative', 30000.00, 6),
+('Marketing and Promotion', 30000.00, 6),
+('Sales', 30000.00, 6);
+
+INSERT INTO employee
+(first_name, last_name, role_id, manager_id)
+VALUES
+('Jeri', 'Smith', 24, 123),
+('Erin', 'Smith', 30, 456),
+('Rapunzel', 'Rider', 14, 789),
+('Mickey', 'Mouse', 1, 001),
+('Minnie', 'Mouse', 19, 002);
+
+-- UPDATE employee SET role_id = 2 WHERE id = 1
+
